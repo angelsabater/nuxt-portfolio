838532c51e18cda45ef7dfece4ae2a90df5f439b
Gel <albsabater.bit@gmail.com>
2019-09-27 17:15:54 +0900

    changed resume url

--- components/Nav.vue
+++ components/Nav.vue
@@ -12,7 +12,7 @@
         <v-toolbar-items class="hidden-sm-and-down">
           <v-btn flat text :to="$t('nav.workLink')"> {{$t('nav.works')}}</v-btn>
           <v-btn flat text :to="$t('nav.aboutLink')"> {{$t('nav.about')}}</v-btn>
-          <v-btn flat text href="https://drive.google.com/file/d/1hXhAUlrdXJqpeqaaM7Sv7kzO3J81pATQ/view?usp=sharing" target="_blank">{{$t('nav.resume')}}</v-btn>
+          <v-btn flat text href="https://drive.google.com/file/d/1jL_HaauxqpKMSTi0qXkAf4pljiBzhtmp/view?usp=sharing" target="_blank">{{$t('nav.resume')}}</v-btn>
         </v-toolbar-items>
         <v-menu class="hidden-md-and-up">
         <v-toolbar-side-icon slot="activator"></v-toolbar-side-icon>
@@ -28,7 +28,7 @@
           </v-row>
           <v-row align="center">
               <div class="my-2">
-                <v-btn flat text href="https://drive.google.com/file/d/1hXhAUlrdXJqpeqaaM7Sv7kzO3J81pATQ/view?usp=sharing" target="_blank">{{$t('nav.resume')}}</v-btn>
+                <v-btn flat text href="https://drive.google.com/file/d/1jL_HaauxqpKMSTi0qXkAf4pljiBzhtmp/view?usp=sharing" target="_blank">{{$t('nav.resume')}}</v-btn>
             </div>
           </v-row>
         </v-menu>
